f6cd56ca7f6beb81d3797caffae7ab41291fee22
Воронин Юрий Сергеевич <yuvoronin@swgroup.ru>
2017-11-03 11:14:58 +0300

    12345

--- b2bSwgroup/Models/ApplicationUserManager.cs
+++ b2bSwgroup/Models/ApplicationUserManager.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using System.Threading.Tasks;
 using System.Linq;
-using b2bSwgroup.Models.ModelsForView;
 
 namespace b2bSwgroup.Models
 {
